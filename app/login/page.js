d947105fc619c0413b5b82008f16c9a0987826d2
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:02:50 +0500

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Login Page</h2>
+      <input placeholder="Email" /><br /><br />
+      <input placeholder="Password" type="password" /><br /><br />
+      <button>Login</button>
+    </div>
+  );
+}
