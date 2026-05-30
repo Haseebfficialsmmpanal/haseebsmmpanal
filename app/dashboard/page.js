bb7a40156edae7a3df401513d6d11195ddf07224
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:05:01 +0500

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Dashboard</h1>
+      <p>Welcome</p>
+      <ul>
+        <li>TikTok</li>
+        <li>YouTube</li>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Free Fire</li>
+      </ul>
+    </div>
+  );
+}
