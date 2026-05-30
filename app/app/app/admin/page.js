8bd8b5b23ee6b02a0f71a0ece8161528c8b73022
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:36:39 +0500

    Create page.js

---
+++ app/app/app/admin/page.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>Admin Panel</h1>
+      <p>Manage Orders & Users</p>
+    </div>
+  );
+}
