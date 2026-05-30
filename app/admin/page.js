b03a33681ec65ca658e96fefc362c1df77374d03
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:53:24 +0500

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,18 @@
+export default function Admin() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>ADMIN PANEL</h1>
+
+      <p>Welcome Admin</p>
+
+      <h3>Manage System</h3>
+
+      <ul>
+        <li>View Users</li>
+        <li>Manage Orders</li>
+        <li>Add Services</li>
+        <li>Check Balance</li>
+      </ul>
+    </div>
+  );
+}
