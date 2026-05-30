910fe189383ab50dbf518fc4562ba32f2feed8da
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:50:19 +0500

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>SMM PANEL</h1>
-      <a href="/login">Login</a>
-    </div>
-  );
-}
