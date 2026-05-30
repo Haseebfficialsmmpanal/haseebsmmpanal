25fd18434ab70f187f8705d41e608e9a0b015c08
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:54:25 +0500

    Create page.js

---
+++ app/payment/page.js
@@ -0,0 +1,13 @@
+export default function Payment() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>PAYMENT PAGE</h1>
+
+      <p>Add Funds / Deposit</p>
+
+      <input placeholder="Enter Amount" /><br/><br/>
+
+      <button>Add Money</button>
+    </div>
+  );
+}
