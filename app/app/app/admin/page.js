684d6267ea9e6ccb80a1411af33a1901d0c3338f
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:48:36 +0500

    Delete app/app directory

--- app/app/app/admin/page.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div style={{textAlign:"center",marginTop:"100px"}}>
-      <h1>Admin Panel</h1>
-      <p>Manage Orders & Users</p>
-    </div>
-  );
-}
