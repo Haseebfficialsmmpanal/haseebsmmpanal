e96da81d0c21974fd14789b8dc8bb06ba86acc0e
Haseebfficialsmmpanal <haseebsmmpanal@gmail.com>
2026-05-30 09:20:42 +0500

    Create page2.js

---
+++ app/page2.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>MY SMM PANEL</h1>
+      <p>Welcome</p>
+      <a href="/login">Login</a>
+    </div>
+  );
+}
